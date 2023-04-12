11e9204923a261313dd582c4ee2188eb673602b5
J0n4th4n6 <130578454+J0n4th4n6@users.noreply.github.com>
2023-04-12 17:54:28 +0200

    Update myfirstscript.R

--- myfirstscript.R
+++ myfirstscript.R
@@ -50,3 +50,41 @@ plot(Alessio, Sherika, cex=2, col="red", pch=11, xlab="shark bite", ylab="gonorr
 # cor() correlation tra due variabili
 cor(Alessio, Sherika)
 
+
+
+#Ex. 2
+
+eta <- c(22, 24, 22, 24, 23, 22, 32, 23, 26, 23, 28, 22, 25)
+#tolgo 41: flavio
+
+#media: somma dei valori diviso il numero dei valori
+#summary() funzione media
+#read.devon() carica dati dall'esterno
+
+summary(eta)
+> summary(eta)
+   Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+  22.00   22.00   23.00   24.31   25.00   32.00 
+#media/mediana (mediana assicura di prendere il valore centrale della serie se abbiamo outliner, 
+#media fa somma di tutto)
+
+etaplus <- c(22, 24, 22, 24, 23, 22, 32, 23, 26, 23, 28, 22, 25, 41, 47)
+summary(etaplus)
+   Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+  22.00   22.50   24.00   26.93   27.00   47.00 
+
+etaplusimpossible <- c(22, 24, 22, 24, 23, 22, 32, 23, 26, 23, 28, 22, 25, 41, 47, 4000)
+summary(etaplusimpossible)
+   Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+  22.00   22.75   24.00  275.25   29.00 4000.00 
+
+#hist() grafico ad istogrammi
+
+#boxplot() indica la mediana
+
+
+
+
+
+
+
